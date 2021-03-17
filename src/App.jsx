2d561da8b141f950db90c1c43060214672516b12
Abhishek Singh <abhishek.singh@abhishekr205.successivetech.com>
2021-03-17 20:34:49 +0530

    task complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextFieldDemo } from './pages';
+
+function App() {
+  return (
+    <div className="App">
+      <TextFieldDemo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextField/style.js
@@ -0,0 +1,9 @@
+export const error = {
+  width: '100%',
+  border: '1px solid red',
+};
+
+export const valid = {
+  width: '100%',
+  border: '1px solid orange',
+};
